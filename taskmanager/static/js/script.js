0434d3066bfca9d27c198afa7e97418b3b8761d1
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-01-11 18:20:43 +0000

    Add functionality to del category and to add new task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var sidenav = document.querySelectorAll('.sidenav');
+    let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: 'dd-mmmm-yyyy',
+        i18n: { done: "Select" }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    let datepicker = document.querySelectorAll('select');
+    M.FormSelect.init(datepicker);
 });
